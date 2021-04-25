7fa6b2c82c0a7d213356f6242230defa65c44a6c
kongxiangchx <773046513@qq.com>
2021-04-25 15:21:17 +0800

    诗词、成语的采集与处理

---
+++ 数据采集/deal_idiom.py
@@ -0,0 +1,62 @@
+import struct
+import os
+
+# 拼音表偏移，
+startPy = 0x1540
+
+# 汉语词组表偏移
+startChinese = 0x2628
+
+GTable = []
+
+
+def byte2str(data):
+    pos = 0
+    str = ''
+    while pos < len(data):
+        c = chr(struct.unpack('H', bytes([data[pos], data[pos + 1]]))[0])
+        if c != chr(0):
+            str += c
+        pos += 2
+    return str
+
+
+def getChinese(data):
+    pos = 0
+    while pos < len(data):
+        same = struct.unpack('H', bytes([data[pos], data[pos + 1]]))[0]
+        pos += 2
+        py_table_len = struct.unpack('H', bytes([data[pos], data[pos + 1]]))[0]
+        pos += 2
+        pos += py_table_len
+        for i in range(same):
+            c_len = struct.unpack('H', bytes([data[pos], data[pos + 1]]))[0]
+            pos += 2
+            word = byte2str(data[pos: pos + c_len])
+            pos += c_len
+            ext_len = struct.unpack('H', bytes([data[pos], data[pos + 1]]))[0]
+            pos += 2
+            if len(word) == 4:
+                GTable.append((word))
+            pos += ext_len
+
+
+def scel2txt(file_name):
+    print('-' * 60)
+    with open(file_name, 'rb') as f:
+        data = f.read()
+    print("词库名：", byte2str(data[0x130:0x338]))  # .encode('GB18030')
+    print("词库类型：", byte2str(data[0x338:0x540]))
+    print("描述信息：", byte2str(data[0x540:0xd40]))
+    print("词库示例：", byte2str(data[0xd40:startPy]))
+    getChinese(data[startChinese:])
+
+
+if __name__ == '__main__':
+    scel2txt('生活常用成语大全.scel')
+    f = open('成语.txt', 'w', encoding='UTF-8-SIG')
+    cnt = 0
+    for word in GTable:
+        f.write(str(cnt)+','+word+'\n')
+        cnt += 1
+    f.close()
---
+++ 数据采集/deal_poem.py
@@ -0,0 +1,64 @@
+import re
+import os
+
+
+def is_rare_name(string):
+    pattern = re.compile(u'[~!@#$%^&* ]')
+    match = pattern.search(string)
+    if match:
+        return True
+    try:
+        string.encode('gb2312')
+    except UnicodeEncodeError:
+        return True
+    return False
+
+
+def judge1(sen):
+    symbol = ['，', '。', '？', '！', '·']
+    for i in sen:
+        for j in i:
+            if j not in symbol and is_rare_name(j):
+                return False
+    return True
+
+
+def judge2(sen):
+    symbol = ['。', '？', '！', '、', '：', '；']
+    num = 0
+    for i in sen:
+        if i in symbol:
+            return False
+        elif i == '，':
+            num += 1
+    if num == 1:
+        return True
+    return False
+
+
+f = open('诗词1.txt', 'r', encoding='UTF-8-SIG')
+f1 = open('诗词2.txt', 'w', encoding='UTF-8-SIG')
+cnt = 0
+unique = set()
+while True:
+    line = f.readline()
+    if line == '':
+        break
+    line = line[:len(line)-1]
+    sen = line.split(',')
+    # print(sen)
+    flag = True
+    for i in sen:
+        if i == '':
+            flag = False
+    if flag:
+        if sen[0].endswith(('。', '？', '！', '；')):
+            sen[0] = sen[0].rstrip('。？！；')
+        if judge1(sen) and judge2(sen[0]) and sen[0] not in unique and len(sen[0]) <= 15 and len(sen[1]) <= 5 and len(sen[2]) <= 15:
+            unique.add(sen[0])
+            f1.write(str(cnt)+',' +
+                     ','.join(sen[0].split('，'))+','+sen[1]+','+sen[2]+'\n')
+            cnt += 1
+f.close()
+f1.close()
+print('共收集名句：', cnt)
